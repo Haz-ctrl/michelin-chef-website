810ebebfc4ca5b5cb778b1de3698f7f824c3eb1f
Hashim Iqbal <iqbalhashim332@gmail.com>
2023-12-20 17:52:35 +0000

    Changes to website as outlined in Website Criticisms N01.pdf Moved the logo to the top left corner Added a splash screen for on load Added a categories section and filter menu.

--- public/script.js
+++ public/script.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Loading overlay animation
+    setTimeout(function() {
+        var loadingOverlay = document.getElementById('loadingOverlay');
+        loadingOverlay.style.top = '-100%'; // Slide up
+    }, 1500); // Adjust the delay as needed
+
+    // Existing code for the burger menu
     var burgerMenu = document.querySelector(".burger-menu");
     var menuPanel = document.getElementById("menuPanel");
     var overlay = document.getElementById("overlay");
@@ -12,23 +19,59 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     function toggleMenu() {
-        if (menuPanel.style.display === "flex") {
-            menuPanel.style.display = "none";
-            overlay.style.opacity = 0;
-            setTimeout(() => { overlay.style.display = "none"; }, 500);
-        } else {
-            menuPanel.style.display = "flex";
+        menuPanel.classList.toggle('open');
+        if (menuPanel.classList.contains('open')) {
             overlay.style.display = "block";
             setTimeout(() => { overlay.style.opacity = 1; }, 50);
+        } else {
+            overlay.style.opacity = 0;
+            setTimeout(() => { overlay.style.display = "none"; }, 500);
         }
     }
-});
 
-document.querySelectorAll('.box-icon').forEach(item => {
-    item.addEventListener('mouseover', () => {
-        item.src = 'img/open_box.png';
+    // Existing code for box icons
+    document.querySelectorAll('.box-icon').forEach(item => {
+        item.addEventListener('mouseover', () => {
+            item.src = 'img/open_box.png';
+        });
+        item.addEventListener('mouseout', () => {
+            item.src = 'img/closed_box.png';
+        });
     });
-    item.addEventListener('mouseout', () => {
-        item.src = 'img/closed_box.png';
+
+    // New functionality to show ingredients
+    var comingSoonItem = document.getElementById("coming-soon-item");
+    if (comingSoonItem) {
+        comingSoonItem.addEventListener("click", function() {
+            showIngredients();
+        });
+    }
+
+    function filterItems(category) {
+        // Get all items
+        var items = document.querySelectorAll('.items-container .item');
+    
+        // Loop through all items
+        items.forEach(function(item) {
+            // If 'all' or the item's class list contains the category, show it
+            if (category === 'all' || item.classList.contains(category)) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none'; // Otherwise, hide it
+            }
+        });
+    }
+
+    function showIngredients() {
+        var ingredientsBox = document.getElementById('ingredients-box');
+        ingredientsBox.style.display = ingredientsBox.style.display === "none" ? "block" : "none";
+    }
+
+    // Optional: Close the ingredients box when clicking outside of it
+    window.addEventListener('click', function(event) {
+        var ingredientsBox = document.getElementById('ingredients-box');
+        if (event.target == ingredientsBox) {
+            ingredientsBox.style.display = "none";
+        }
     });
 });
